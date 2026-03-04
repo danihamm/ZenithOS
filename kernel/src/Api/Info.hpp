ec820884be085b0950795bb586bd289b6adc5986
Daniel Hammer <daniel@nexlink.cloud>
2026-03-04 14:29:49 +0100

    feat: increment version number

--- kernel/src/Api/Info.hpp
+++ kernel/src/Api/Info.hpp
@@ -16,7 +16,7 @@ namespace Montauk {
 
         // Copy strings into fixed-size arrays (user-accessible)
         const char* name = "MontaukOS";
-        const char* ver = "0.1.0";
+        const char* ver = "0.1.1";
         for (int i = 0; name[i]; i++) outInfo->osName[i] = name[i];
         outInfo->osName[9] = '\0';
         for (int i = 0; ver[i]; i++) outInfo->osVersion[i] = ver[i];
